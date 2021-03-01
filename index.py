2fa27de3c885ac4e4417c329792f76b43e97ce19
davidRamalho <david.m.ramalho@gmail.com>
2021-03-01 15:27:10 -0700

    fail with bs4

--- index.py
+++ index.py
@@ -1,18 +1,23 @@
 from urllib.request import urlopen
+import requests
 from bs4 import BeautifulSoup
 
+r = requests.get("https://cache.dominos.com/nolo/ca/en/6_44_5/assets/build/market/CA/_en/templates/nationalCouponPageComponent.js")
 url_to_scrape = "https://www.dominos.ca/en/pages/order/coupon"
 
+print(r.text)
 request_page = urlopen(url_to_scrape)
 page_html = request_page.read()
+
 request_page.close()
 
 soup = BeautifulSoup(page_html, 'html.parser')
 
-promo_items = soup.find_all('a', class_="js-addCoupon")
+promo_items = soup.find('div', id="js-nationalCouponPage")
+print(promo_items)
 
+# This is not going to work.... :( - hidden HTML elements  - Will try Selenium. 
 
-
-for promo in promo_items:
-  img = promo.find('img')
-  print(promo)
+# for promo in promo_items:
+#   img = promo.find('img')
+#   print(promo)

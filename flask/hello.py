479aca9001d091c2d5b2fd282ea4f6d18fa284c1
davidRamalho <david.m.ramalho@gmail.com>
2021-04-02 23:28:02 -0600

    adding templates

--- flask/hello.py
+++ flask/hello.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route('/about')
-def about():
-    return render_template("about.html")
+@app.route('/home')
+def home():
+    return render_template("home.html")
